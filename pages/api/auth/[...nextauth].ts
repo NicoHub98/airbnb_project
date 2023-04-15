3d04551ab1bc5bd7eb417c8468e5b10dbcf69e98
NicoHub98 <nicolud98@gmail.com>
2023-04-15 01:43:01 -0300

    sec

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -11,12 +11,12 @@ export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
+      clientId: process.env.REACT_APP_GITHUB_ID as string,
+      clientSecret: process.env.REACT_APP_GITHUB_SECRET as string,
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.REACT_APP_GOOGLE_CLIENT_SECRET as string,
     }),
     CredentialsProvider({
       name: "credentials",
